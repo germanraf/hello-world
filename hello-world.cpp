b578f7711e8266c155fcc291e05cd44218467964
germanraf <76071474+germanraf@users.noreply.github.com>
2020-12-19 13:59:55 +0300

    Add files via upload

---
+++ hello-world.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello-world
+//
+//  Created by German on 19/12/2020.
+//  Copyright © 2020 germanraf. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    // my new way to new world...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
